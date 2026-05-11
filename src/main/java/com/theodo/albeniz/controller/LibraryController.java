6d71e9129f3570b1f2df17e4297c15961a2e7922
Gatien TIREL <gatien.tirel@theodo.com>
2026-05-11 16:45:54 +0200

    feature(TuneDto): new dto + use in Library controller

--- src/main/java/com/theodo/albeniz/controller/LibraryController.java
+++ src/main/java/com/theodo/albeniz/controller/LibraryController.java
@@ -7,12 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.theodo.albeniz.dto.Tune;
+
 @RestController()
 @RequestMapping("/library")
 public class LibraryController {
 
     @GetMapping("/music")
-    public Collection<String> getMusic() {
-        return Arrays.asList("Blip", "Blap");
+    public Collection<Tune> getMusic() {
+        return Arrays.asList(new Tune("Blip", "Zizou"), new Tune("Blap", "Schuman"));
     }
 }
