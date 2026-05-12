717f07846db0d3442f051b1d6978b983a3e910bb
Gatien TIREL <gatien.tirel@theodo.com>
2026-05-12 17:51:09 +0200

    feature(LibraryService): service + unit tests

--- src/test/java/com/theodo/albeniz/controller/LibraryControllerTest.java
+++ src/test/java/com/theodo/albeniz/controller/LibraryControllerTest.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Description;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.theodo.albeniz.dto.Tune;
+import com.theodo.albeniz.services.LibraryService;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -23,56 +25,55 @@ import org.junit.jupiter.api.Test;
 
 @WebMvcTest()
 @AutoConfigureMockMvc()
+@Import({ LibraryService.class })
 public class LibraryControllerTest {
-    @Autowired
-    private MockMvc mockMvc;
+        @Autowired
+        private MockMvc mockMvc;
 
-    @Test()
-    @Description("it should return the full library of music")
-    public void testGetMusicsRoute() throws Exception {
-        ObjectMapper jsonMapper = new ObjectMapper();
-        Collection<Tune> expectedFullLibrary = List.of(
-                new Tune(1, "Thriller", "Michael J."),
-                new Tune(2, "Uptown Funk", "Bruno M."),
-                new Tune(3, "The Little Foam Man", "Patrick S."));
-        mockMvc.perform(get("/library/music")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().json(
-                        "[{\"id\": 1,\"title\": \"Thriller\",\"author\": \"Michael J.\"},"
-                                + "{\"id\": 2,\"title\": \"Uptown Funk\",\"author\": \"Bruno M.\"},"
-                                + "{\"id\": 3,\"title\": \"The Little Foam Man\",\"author\": \"Patrick S.\"}]"));
-    }
+        @Test()
+        @Description("it should return the full library of music")
+        public void testGetMusicsRoute() throws Exception {
+                ObjectMapper jsonMapper = new ObjectMapper();
+                Collection<Tune> expectedFullLibrary = List.of(
+                                new Tune(1, "Thriller", "Michael J."),
+                                new Tune(2, "Uptown Funk", "Bruno M."),
+                                new Tune(3, "The Little Foam Man", "Patrick S."));
+                mockMvc.perform(get("/library/music")
+                                .contentType(MediaType.APPLICATION_JSON))
+                                .andExpect(status().isOk())
+                                .andExpect(content().json(jsonMapper.writeValueAsString(expectedFullLibrary)));
+        }
 
-    @Test()
-    @Description("it should returns the information for an existing music")
-    public void testGetOneMusic() throws Exception {
-        ObjectMapper jsonMapper = new ObjectMapper();
-        Tune expectedTune = new Tune(2, "Uptown Funk", "Bruno M.");
-        mockMvc.perform(get("/library/music/2")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().json(jsonMapper.writeValueAsString(expectedTune)));
-    }
+        @Test()
+        @Description("it should returns the information for an existing music")
+        public void testGetOneMusic() throws Exception {
+                ObjectMapper jsonMapper = new ObjectMapper();
+                Tune expectedTune = new Tune(2, "Uptown Funk", "Bruno M.");
+                mockMvc.perform(get("/library/music/2")
+                                .contentType(MediaType.APPLICATION_JSON))
+                                .andExpect(status().isOk())
+                                .andExpect(content().json(jsonMapper.writeValueAsString(expectedTune)));
+        }
 
-    @Test()
-    @Description("it should return an empty JSON string for an non-existing music")
-    public void testGetNotExistingMusic() throws Exception {
-        MvcResult mvcResponse = mockMvc.perform(get("/library/music/14")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk()).andReturn();
-        String mvcResponseContentStr = mvcResponse.getResponse().getContentAsString();
-        assertEquals("", mvcResponseContentStr);
-    }
+        @Test()
+        @Description("it should return an empty JSON string for an non-existing music")
+        public void testGetNotExistingMusic() throws Exception {
+                MvcResult mvcResponse = mockMvc.perform(get("/library/music/14")
+                                .contentType(MediaType.APPLICATION_JSON))
+                                .andExpect(status().isOk()).andReturn();
+                String mvcResponseContentStr = mvcResponse.getResponse().getContentAsString();
+                assertEquals("", mvcResponseContentStr);
+        }
 
-    @Test()
-    @Description("it should return musics where title contains 'up' (not case-sensitive) ")
-    public void testGetMusicsWhereTitleContainsUp() throws Exception {
-        ObjectMapper jsonMapper = new ObjectMapper();
-        Collection<Tune> expectedTunes = List.of(new Tune(2, "Uptown Funk", "Bruno M."));
-        MvcResult mvcResponse = mockMvc.perform(get("/library/music?title=up").contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk()).andReturn();
-        String mvcContentStr = mvcResponse.getResponse().getContentAsString();
-        assertEquals(mvcContentStr, jsonMapper.writeValueAsString(expectedTunes));
-    }
+        @Test()
+        @Description("it should return musics where title contains 'up' (not case-sensitive) ")
+        public void testGetMusicsWhereTitleContainsUp() throws Exception {
+                ObjectMapper jsonMapper = new ObjectMapper();
+                Collection<Tune> expectedTunes = List.of(new Tune(2, "Uptown Funk", "Bruno M."));
+                MvcResult mvcResponse = mockMvc
+                                .perform(get("/library/music?title=up").contentType(MediaType.APPLICATION_JSON))
+                                .andExpect(status().isOk()).andReturn();
+                String mvcContentStr = mvcResponse.getResponse().getContentAsString();
+                assertEquals(mvcContentStr, jsonMapper.writeValueAsString(expectedTunes));
+        }
 }
---
+++ src/test/java/com/theodo/albeniz/services/LibraryServiceTest.java
@@ -0,0 +1,51 @@
+package com.theodo.albeniz.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.Description;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.theodo.albeniz.dto.Tune;
+
+public class LibraryServiceTest {
+
+    @Test()
+    @Description("it should return all tunes in library")
+    void testGetAllMethod() throws Exception {
+        LibraryService service = new LibraryService();
+        ObjectMapper mapper = new ObjectMapper();
+        Collection<Tune> expectedLibrary = List.of(
+                new Tune(1, "Thriller", "Michael J."),
+                new Tune(2, "Uptown Funk", "Bruno M."),
+                new Tune(3, "The Little Foam Man", "Patrick S."));
+        assertEquals(
+                mapper.writeValueAsString(service.getAll(null)),
+                mapper.writeValueAsString(expectedLibrary));
+    }
+
+    @Test()
+    @Description("it should return all tunes in library with title containing 'foam' ")
+    void testGetAllWithQueryMethod() throws Exception {
+        LibraryService service = new LibraryService();
+        ObjectMapper mapper = new ObjectMapper();
+        Collection<Tune> expectedTunes = List.of(new Tune(3, "The Little Foam Man", "Patrick S."));
+        assertEquals(
+                mapper.writeValueAsString(service.getAll("foam")),
+                mapper.writeValueAsString(expectedTunes));
+    }
+
+    @Test()
+    @Description("it should return tune with id 1")
+    void testGetOneMethod() throws Exception {
+        LibraryService service = new LibraryService();
+        ObjectMapper mapper = new ObjectMapper();
+        Tune expectedTune = new Tune(1, "Thriller", "Michael J.");
+        assertEquals(
+                mapper.writeValueAsString(service.getOne(1)),
+                mapper.writeValueAsString(expectedTune));
+    }
+}
